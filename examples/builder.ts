b00d40b3243ec25a010f9a9d6efa3a61cbe24ac8
Leon Rinkel <leon@rinkel.me>
2019-12-03 22:13:16 +0100

    ➕ Add domainDescFromXml

--- examples/builder.ts
+++ examples/builder.ts
@@ -1,66 +1,46 @@
-import { domainDescToXml, DomainDesc, DomainBuilder } from "../dist";
+import { domainDescToXml, DomainDesc, DomainBuilder, domainDescFromXml } from "../dist";
 
-const template: DomainDesc = {
+(async () => {
 
-    type: "kvm",
+    const template = await domainDescFromXml(
+`<domain type="kvm">
+    <memory>1048576</memory>
+    <currentMemory>1048576</currentMemory>
+    <vcpu>1</vcpu>
+    <os>
+        <type arch="x86_64" machine="q35">hvm</type>
+        <boot dev="hd"/>
+    </os>
+    <devices>
+        <emulator>/usr/bin/qemu-system-x86_64</emulator>
+        <console type="pty"/>
+        <graphics type="vnc" port="-1" listen="0.0.0.0" passwd="test1"/>
+    </devices>
+</domain>`);
 
-    memory: { value: 1048576 },
-    currentMemory: { value: 1048576 },
+    const domain = new DomainBuilder()
+        .fromTemplate(template)
 
-    vcpu: { value: 1 },
+        .setName("test1")
+        .setUUID("148d0864-2354-4c27-b82c-731bdd3f320c")
 
-    os: {
-        type: { arch: "x86_64", machine: "q35", value: "hvm" },
-        boot: { dev: "hd" },
-    },
+        .addDisk({
+            type: "file", device: "disk",
+            driver: { name: "qemu", type: "qcow2" },
+            source: { file: "/home/leon/test1.img" },
+            target: { dev: "vda", bus: "virtio" },
+        })
 
-    devices: [
+        .addInterface({
+            type: "network",
+            source: { network: "default" },
+            mac: { address: "52:54:00:8e:c6:5f" },
+            model: { type: "virtio" },
+        })
 
-        {
-            type: "emulator",
-            emulator: { value: "/usr/bin/qemu-system-x86_64" },
-        },
+        .build();
 
-        {
-            type: "console",
-            console: { type: "pty" },
-        },
+        const xml = domainDescToXml(domain);
+        console.log(xml);
 
-        {
-            type: "graphics",
-            graphics: {
-                type: "vnc",
-                port: -1,
-                listen: "0.0.0.0",
-                passwd: "test1",
-            }
-        },
-
-    ],
-
-};
-
-const domain = new DomainBuilder()
-    .fromTemplate(template)
-
-    .setName("test1")
-    .setUUID("148d0864-2354-4c27-b82c-731bdd3f320c")
-
-    .addDisk({
-        type: "file", device: "disk",
-        driver: { name: "qemu", type: "qcow2" },
-        source: { file: "/home/leon/test1.img" },
-        target: { dev: "vda", bus: "virtio" },
-    })
-
-    .addInterface({
-        type: "network",
-        source: { network: "default" },
-        mac: { address: "52:54:00:8e:c6:5f" },
-        model: { type: "virtio" },
-    })
-
-    .build();
-
-const xml = domainDescToXml(domain);
-console.log(xml);
+})();
