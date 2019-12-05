b4ab28cc2a047376000a81c3ece025f7ab5904db
libvirtadept <ilya_b@hip-hosting.com>
2019-12-05 13:49:39 +0300

    up

--- examples/interface_stats.ts
+++ examples/interface_stats.ts
@@ -1,42 +1,29 @@
 import chalk from "chalk";
 import process from "process";
+import util from 'util';
 
 import libvirt from "../";
 
 (async () => {
+    const vmName = "debian9-vm3", // VM name
+          ifName = "vnet2";        // VM Interface Name
 
     const uri = "qemu+ssh://root@libvirt_dev/system";
     const hypervisor = new libvirt.Hypervisor({ uri });
 
     await hypervisor.connectOpen();
-    console.time('s');
+
     const hostname = await hypervisor.connectGetHostname();
     process.stdout.write(`Connected to ${hostname}!\n\n`);
 
-    const activeDomains = await hypervisor.connectListAllDomains(
-        libvirt.ConnectListAllDomainsFlags.ACTIVE);
-    const inactiveDomains = await hypervisor.connectListAllDomains(
-        libvirt.ConnectListAllDomainsFlags.INACTIVE);
-
-    const activeDomainNames = await Promise.all(activeDomains
-        .map((domain) => hypervisor.domainGetName(domain)));
-
-    const inactiveDomainNames = await Promise.all(inactiveDomains
-        .map((domain) => hypervisor.domainGetName(domain)));
-    await hypervisor.connectClose();
-    console.timeEnd('s')
-    process.stdout.write("Active Domains\n");
-    process.stdout.write("--------------\n");
-    for (const name of activeDomainNames) {
-        process.stdout.write(chalk.green(`  ${name}\n`));
+    const activeDomain = await hypervisor.domainLookupByName(vmName).then(domain => domain).catch(() => null); // Domain not exist or error
+    if (!activeDomain){
+        console.log(`Error on lookup for ${chalk.blue(vmName)}`);
+        return;
     }
-    process.stdout.write("\n");
 
-    process.stdout.write("Inactive Domains\n");
-    process.stdout.write("-----------------\n");
-    for (const name of inactiveDomainNames) {
-        process.stdout.write(chalk.red(`  ${name}\n`));
-    }
-    process.stdout.write("\n");
+    const ifStats = await hypervisor.domainInterfaceStats(activeDomain, ifName);
+    console.log(`Domain ${chalk.green(vmName)} interface stats of ${chalk.green(ifName)} interface:`);
 
+    console.log(util.inspect(ifStats, { colors: true, sorted: true }));
 })();
